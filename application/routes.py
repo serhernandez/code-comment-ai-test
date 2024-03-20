56cbed94111db7c8be6835eaf2ae011ff7295af7
serhernandez <sergio.hernandez@cgi.com>
2024-03-20 11:47:57 -0500

    Add ability to change models between gpt 3.5 turbo, gpt 4, and gpt 4 turbo preview

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from flask import current_app as app
-from flask import render_template, request
+from flask import render_template, request, session
 
 from .clients import openai_client
 
@@ -7,7 +7,9 @@ import markdown
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    if 'current_model' not in session:
+        session['current_model'] = app.config['OPENAI_MODELS'][0]
+    return render_template("index.html", models=app.config['OPENAI_MODELS'], curmodel=session['current_model'])
 
 @app.route("/generatecomments", methods=['POST'])
 def generate_comments():
@@ -15,9 +17,18 @@ def generate_comments():
     submittedCode = request.form.get('userInput')
     formattedInput = {"role": "user", "content": submittedCode}
     messages.append(formattedInput)
-    completion = openai_client.chat.completions.create(model='gpt-4-turbo-preview', messages=messages, max_tokens=4096)
+    completion = openai_client.chat.completions.create(model=session['current_model'], messages=messages, max_tokens=4096)
     response = completion.choices[0].message.content
     formatted_resp = markdown.markdown(response, extensions=['codehilite', 'fenced_code'])
     if app.debug:
         print(f"Sent {completion.usage.prompt_tokens} tokens and received {completion.usage.completion_tokens} tokens back")
     return formatted_resp
+
+@app.route("/selectmodel", methods=['PUT'])
+def change_model():
+    new_model = request.form.get('model')
+    if new_model in app.config['OPENAI_MODELS']:
+        session['current_model'] = new_model
+    if app.debug:
+        print(f"Model changed to {new_model} successfully")
+    return '', 204
--- config.py
+++ config.py
@@ -6,4 +6,5 @@ load_dotenv()
 
 class Config:
     OPENAI_API_KEY = getenv("OPENAI_API_KEY", "")
+    OPENAI_MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4-turbo-preview"]
     SECRET_KEY = token_hex(16)
