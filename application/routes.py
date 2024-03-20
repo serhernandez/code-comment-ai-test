bcae2b8789e604b0efe8cf7b9133c2edb7e63161
serhernandez <sergio.hernandez@cgi.com>
2024-03-20 11:00:59 -0500

    Add functionality to comment code submitted by the user. Currently defaults to gpt 4 turbo.

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,23 @@
 from flask import current_app as app
-from flask import render_template
+from flask import render_template, request
 
 from .clients import openai_client
+
+import markdown
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/generatecomments", methods=['POST'])
+def generate_comments():
+    messages = [{"role": "system", "content": "You are an AI assistant who will receive code from the user and output the code with helpful comments added explaining what the code is doing. Please do not include extra comments outside of the returned code block."}]
+    submittedCode = request.form.get('userInput')
+    formattedInput = {"role": "user", "content": submittedCode}
+    messages.append(formattedInput)
+    completion = openai_client.chat.completions.create(model='gpt-4-turbo-preview', messages=messages, max_tokens=4096)
+    response = completion.choices[0].message.content
+    formatted_resp = markdown.markdown(response, extensions=['codehilite', 'fenced_code'])
+    if app.debug:
+        print(f"Sent {completion.usage.prompt_tokens} tokens and received {completion.usage.completion_tokens} tokens back")
+    return formatted_resp
