1548a543e0e529bd75f8fd966df1185f89c742ab
serhernandez <sergio.hernandez@cgi.com>
2024-03-21 17:53:59 -0500

    Add a download button to allow the user to download the raw markdown

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from flask import current_app as app
-from flask import render_template, request, session
+from flask import jsonify, render_template, request, session
 
 from .clients import openai_client
 
@@ -13,7 +13,9 @@ def home_page():
 
 @app.route("/generatecomments", methods=['POST'])
 def generate_comments():
-    messages = [{"role": "system", "content": "You are an AI assistant who will receive code from the user and output the code with helpful comments added explaining what the code is doing. Please do not include extra comments outside of the returned code block."}]
+    #messages = [{"role": "system", "content": "You are an AI assistant who will receive code from the user and output the code with helpful comments added explaining what the code is doing. Please do not include extra comments outside of the returned code block."}]
+    #messages = [{"role": "system", "content": "Make a document that goes over the code provided. Please make it detailed and add the params and return values for all methods. "}]
+    messages = [{"role": "system", "content": "Hello! As a computer scientist, I am seeking your expertise in interpreting and documenting code specifically for Python and SAS languages. When I provide you with code snippets, my expectation is for you to analyze them and then create documentation that clearly explains their functionality, parameters, returns, and any exceptions they might raise. For Python code, please ensure that the documentation follows the official Python documentation style, adhering to docstring conventions and PEP 257. For SAS code, documentation should be similarly detailed, including explanations of data steps, proc steps, and any macros, with clear descriptions of the purpose, usage, parameters, and outputs.The goal is to produce documentation that is both comprehensive and accessible, making it easy for developers of various skill levels to understand and use the code effectively. Could you please assist me with this task?"}]
     submittedCode = request.form.get('userInput')
     formattedInput = {"role": "user", "content": submittedCode}
     messages.append(formattedInput)
@@ -22,7 +24,7 @@ def generate_comments():
     formatted_resp = markdown.markdown(response, extensions=['codehilite', 'fenced_code'])
     if app.debug:
         print(f"Sent {completion.usage.prompt_tokens} tokens and received {completion.usage.completion_tokens} tokens back")
-    return formatted_resp
+    return jsonify([formatted_resp, response])
 
 @app.route("/selectmodel", methods=['PUT'])
 def change_model():
